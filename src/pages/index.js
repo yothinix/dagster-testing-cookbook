1368078963268b914ffbcaae804cb6e3e067d804
Yothin Muangsommuk <yothin.m@linecorp.com>
2022-10-06 22:04:57 +0700

    Custom main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="A recipe for testing in Dagster <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
